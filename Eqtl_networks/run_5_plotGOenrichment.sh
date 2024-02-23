5a3d255656fcde3ec15a9172084c95d406a3dca2
maudf <maud.fagny@ens-lyon.org>
2024-02-23 12:35:19 +0100

    Update run_5_plotGOenrichment.sh

--- Eqtl_networks/run_5_plotGOenrichment.sh
+++ Eqtl_networks/run_5_plotGOenrichment.sh
@@ -16,11 +16,53 @@
 #SBATCH --time=3-00:00:00 # days-hh:mm:ss
 
 ### Set variables
+Rscript enrichment_cluster_topGO.R \
+-d main/ \
+-t Brain_Nucleus_accumbens_basal_ganglia \
+-a Data/Annotations/tissues_annotation.Rds \
+-m 5 \
+-p 0.01 \
+-i Results/GOanalysis/
 
 Rscript enrichment_cluster_topGO.R \
 -d main/ \
--t Adipose_Subcutaneous \
+-t Colon_Transverse \
+-a Data/Annotations/tissues_annotation.Rds \
+-m 149 \
+-p 0.01 \
+-i Results/GOanalysis/
+
+Rscript enrichment_cluster_topGO.R \
+-d main/ \
+-t Adipose_Visceral_Omentum \
 -a Data/Annotations/tissues_annotation.Rds \
 -m 142 \
+-p 0.01 \
+-i Results/GOanalysis/
+
+
+Rscript enrichment_cluster_topGO.R \
+-d main/ \
+-t Muscle_Skeletal \
+-a Data/Annotations/tissues_annotation.Rds \
+-m 90 \
+-p 0.01 \
+-i Results/GOanalysis/
+
+
+Rscript enrichment_cluster_topGO.R \
+-d main/ \
+-t Whole_Blood \
+-a Data/Annotations/tissues_annotation.Rds \
+-m 17 \
+-p 0.01 \
+-i Results/GOanalysis/
+
+
+Rscript enrichment_cluster_topGO.R \
+-d main/ \
+-t Brain_Nucleus_accumbens_basal_ganglia \
+-a Data/Annotations/tissues_annotation.Rds \
+-m 100 \
 -p 0.01 \
 -i Results/GOanalysis/
